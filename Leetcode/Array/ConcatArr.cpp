036d77a08d9b0ce100836897947ff5feb2d5bdb5
AmitrajitDas <amitrajitdas31@gmail.com>
2021-08-26 19:40:16 +0530

    different method

--- Leetcode/Array/ConcatArr.cpp
+++ Leetcode/Array/ConcatArr.cpp
@@ -4,12 +4,13 @@ public:
     vector<int> getConcatenation(vector<int> &nums)
     {
         int n = nums.size();
+        vector<int> ans;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < (2 * n); i++)
         {
-            nums.push_back(nums[i]);
+            ans.push_back(nums[i % n]);
         }
 
-        return nums;
+        return ans;
     }
 };
