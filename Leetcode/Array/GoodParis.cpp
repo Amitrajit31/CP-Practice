0192683f9f3b91d1a25ab0a7f9e8d437942cfc4a
AmitrajitDas <amitrajitdas31@gmail.com>
2021-08-28 15:48:55 +0530

    number of good pairs

---
+++ Leetcode/Array/GoodParis.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int numIdenticalPairs(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        int res = 0;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (i < j && nums[i] == nums[j])
+                {
+                    res++;
+                }
+            }
+        }
+
+        return res;
+    }
+};
