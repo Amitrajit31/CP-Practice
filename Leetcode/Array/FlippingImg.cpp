08fdf4dd1a5f84bc3059ac29a19d86051def6b34
AmitrajitDas <amitrajitdas31@gmail.com>
2021-08-30 13:36:22 +0530

    Flipping an Image

---
+++ Leetcode/Array/FlippingImg.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>> &image)
+    {
+        for (int i = 0; i < image.size(); i++)
+        {
+            int low = 0, high = image.size() - 1;
+
+            while (low <= high)
+            {
+                if (image[i][low] == image[i][high])
+                {
+                    image[i][low] = 1 - image[i][low];
+                    image[i][high] = image[i][low];
+                }
+
+                low++;
+                high--;
+            }
+        }
+
+        return image;
+    }
+};
