642abc0d8002d542c7beb4ea4b34647b94b61334
AmitrajitDas <amitrajitdas31@gmail.com>
2021-09-15 14:15:16 +0530

    Valid Perfect Square

---
+++ Leetcode/Searching/ValidPerfectSquare.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    bool isPerfectSquare(int num)
+    {
+        long long int start = 1, end = num;
+
+        while (start <= end)
+        {
+            long long int mid = start + (end - start) / 2;
+
+            if (mid * mid == num)
+            {
+                return true;
+            }
+            else if (mid * mid > num)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
